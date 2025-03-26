05c98e8e3001c5cdca02a9d2fafb8c3c1c70cbc3
siltea2019 <siltea2019@gmail.com>
2025-03-27 03:50:29 +0900

    수업실습: section02 - nextjs 앱 프로젝트 생성 및 Page Router 기초 실습

---
+++ section02/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // React strict mode를 끄는 옵션
+  // 실습을 위해 turn off : 개발모드 실행시 컴포넌트 2번실행
+  reactStrictMode: false,
+};
+
+export default nextConfig;
